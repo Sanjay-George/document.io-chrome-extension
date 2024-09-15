94a2d39fc3c316fd6848964b3d1d2b23f62b59cf
Sanjay George <sanjaygeorge16@gmail.com>
2024-09-15 18:34:25 +0530

    Fix CSS issues
    - Fix the problem where z index was not applied correctly to the overlay. set position sticky to fix this.
    
    - Remove unnecessary css applied on body, which modified the host css

--- background.js
+++ background.js
@@ -6,27 +6,6 @@ chrome.runtime.onInstalled.addListener(() => {
         title: "Add / Edit Annotation",
         contexts: ["all"]
     });
-
-    // Listen for when the user clicks on the context menu item
-    chrome.contextMenus.onClicked.addListener((info, tab) => {
-        if (info.menuItemId === "addEditAnnotation") {
-            // Send message to content script to open the modal
-            chrome.tabs.sendMessage(tab.id, { action: "openModal", targetElement: info.targetElementId });
-            console.log('Context menu - add / edit annotation clicked');
-        }
-    });
-
-    // // Function to send the ping request
-    // const sendPingRequest = () => {
-    //     console.log('Sending ping request...');
-    //     fetch('http://localhost:5000/pages/?documentationId=66d70df18125285844d41c3f')
-    //         .then(response => response.json())
-    //         .then(data => console.log('Ping response:', data))
-    //         .catch(error => console.error('Error:', error));
-    // };
-
-    // // Send the ping request every 30 seconds
-    // setInterval(sendPingRequest, 30000); // 30 seconds = 30,000 milliseconds
 });
 
 // Access the URL of the active tab
@@ -34,5 +13,20 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     let currentTab = tabs[0]; // Get the active tab
     if (currentTab) {
         console.log("Current URL: ", currentTab.url); // Logs the current tab's URL
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+        console.log('Current tab:', tab.url);
+    }
+});
+
+// Listen for when the user clicks on the context menu item
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "addEditAnnotation") {
+        console.log('Context menu - add / edit annotation clicked');
+        // Send message to content script to open the modal
+        chrome.tabs.sendMessage(tab.id, { action: "annotate", targetElement: info.targetElementId });
     }
 });
