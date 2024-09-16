5667d974e41be538a9e3d5d1c1b6a459159c4d32
Sanjay George <sanjaygeorge16@gmail.com>
2024-09-16 17:45:18 +0530

    Update editor modal
    - Save data (insert or update)
    
    - Fetch data if annotationId is present (edit mode)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,11 +12,27 @@ import SaveIcon from './components/save_icon';
 function App() {
     const [annotation, setAnnotation] = useState('');
     const [show, setShow] = useState(false);
+    const [annotationMeta, setAnnotationMeta] = useState(null as any);
 
 
-    const handleOpenModalMessage = (_: MessageEvent) => {
+    const handleOpenModalMessage = async (event: MessageEvent) => {
         // query selector will be an input
         // Fetch annotations from the server
+        const { annotationId, target, url, pageId } = event.data;
+
+        console.log('Open modal message parameters:', { annotationId, target, url, pageId });
+
+        if (annotationId) {
+            try {
+                const res = await fetch(`http://localhost:5000/annotations/${annotationId}`);
+                const data = await res.json();
+                data && setAnnotation(data.value);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+        setAnnotationMeta({ id: annotationId, target, url, pageId } as any);
         setShow(true);
     }
 
@@ -24,6 +40,50 @@ function App() {
         setShow(false);
         postMessage({ action: 'closeModal' });
     }
+
+    const handleSave = async () => {
+        const annotationObj = {
+            target: annotationMeta?.target,
+            url: annotationMeta?.url,
+            value: annotation,
+            pageId: annotationMeta?.pageId,
+        } as any;
+
+        if (annotationMeta?.id) {
+            // Update annotation
+            annotationObj['id'] = annotationMeta.id;
+            try {
+                await fetch(`http://localhost:5000/annotations/${annotationMeta.id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(annotationObj)
+                });
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+        else {
+            // Insert annotation
+            try {
+                await fetch(`http://localhost:5000/annotations`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(annotationObj)
+                });
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+        setShow(false);
+        postMessage({ action: 'closeModal' });
+    }
+
 
     window.onmessage = (event) => {
         const { action } = event.data;
@@ -39,12 +99,12 @@ function App() {
                 style={{ position: "fixed", bottom: 0, left: 0, height: '100vh', width: '100vw' }}
             >
                 <ResizablePanel className="transparent-panel"></ResizablePanel>
-                <ResizableHandle withHandle className='overlay-handle bg-emerald-800' />
+                <ResizableHandle withHandle className='overlay-handle hover:bg-emerald-800' />
                 <ResizablePanel className='overlay-panel' minSize={25}>
                     <div style={{ height: '100%', width: '100%' }} className='p-8'>
                         <MDEditor height='90%' value={annotation} onChange={setAnnotation as any} preview='live' />
                         <div className='flex justify-end space-x-2'>
-                            <button id='dce-modal-save' className='btn mt-2 mb-2 bg-emerald-700 text-white'>
+                            <button onClick={handleSave} id='dce-modal-save' className='btn mt-2 mb-2 bg-emerald-700 text-white'>
                                 <SaveIcon />
                             </button>
                             <button onClick={handleClose} id='dce-modal-close' className='btn mt-2 mb-2 bg-slate-700 text-white text-sm'>
